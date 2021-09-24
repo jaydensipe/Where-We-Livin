d7f1baa4f2d15dba7044919fdb5b9e7202aab7f3
Jayden Sipe <jayden.sipe@gmail.com>
2021-09-24 17:30:57 -0400

    Initial commit

---
+++ ServerEnter.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WhereWeLivin
+{
+    partial class ServerEnter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServerEnter));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.hostButton = new System.Windows.Forms.Button();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 109);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Forte", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 109);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WhereWeLivin?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.Font = new System.Drawing.Font("Forte", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(0, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(527, 119);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Server Address:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // serverTextBox
+            // 
+            this.serverTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverTextBox.Location = new System.Drawing.Point(172, 200);
+            this.serverTextBox.Name = "serverTextBox";
+            this.serverTextBox.Size = new System.Drawing.Size(176, 26);
+            this.serverTextBox.TabIndex = 2;
+            this.serverTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.Font = new System.Drawing.Font("Forte", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label3.Location = new System.Drawing.Point(0, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(527, 119);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Port Number:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.connectButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.connectButton.FlatAppearance.BorderSize = 0;
+            this.connectButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.connectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton.Font = new System.Drawing.Font("Forte", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Image = ((System.Drawing.Image)(resources.GetObject("connectButton.Image")));
+            this.connectButton.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.connectButton.Location = new System.Drawing.Point(159, 372);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(206, 53);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = false;
+            // 
+            // hostButton
+            // 
+            this.hostButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.hostButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.hostButton.FlatAppearance.BorderSize = 0;
+            this.hostButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.hostButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hostButton.Font = new System.Drawing.Font("Forte", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hostButton.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.hostButton.Location = new System.Drawing.Point(397, 123);
+            this.hostButton.Name = "hostButton";
+            this.hostButton.Size = new System.Drawing.Size(118, 44);
+            this.hostButton.TabIndex = 6;
+            this.hostButton.Text = "Host";
+            this.hostButton.UseVisualStyleBackColor = false;
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portTextBox.Location = new System.Drawing.Point(172, 313);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(176, 26);
+            this.portTextBox.TabIndex = 7;
+            this.portTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ServerEnter
+            // 
+            this.AcceptButton = this.connectButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 466);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.hostButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.serverTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ServerEnter";
+            this.Text = "WhereWeLivin";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox portTextBox;
+
+        private System.Windows.Forms.TextBox serverTextBox;
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button hostButton;
+
+        private System.Windows.Forms.Button confirmButton;
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Panel panel1;
+
+        #endregion
+    }
+}
